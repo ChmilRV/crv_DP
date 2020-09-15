40971646a136837db4525620be56dd2cbefb0a18
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-09-15 15:44:35 +0300

    1544

--- DesignPatterns_Decorator/Program.cs
+++ DesignPatterns_Decorator/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using static System.Console;
+/*https://refactoring.guru/ru/design-patterns/decorator
+Декоратор — это структурный паттерн проектирования,
+который позволяет динамически добавлять объектам новую функциональность,
+оборачивая их в полезные «обёртки».*/
 namespace DesignPatterns_Decorator
 {
-    // Базовый интерфейс Компонента определяет поведение, которое изменяется
-    // декораторами.
     public abstract class Component
     {
         public abstract string Operation();
     }
-    // Конкретные Компоненты предоставляют реализации поведения по умолчанию.
-    // Может быть несколько вариаций этих классов.
     class ConcreteComponent : Component
     {
         public override string Operation()
@@ -16,52 +17,31 @@ namespace DesignPatterns_Decorator
             return "ConcreteComponent";
         }
     }
-    // Базовый класс Декоратора следует тому же интерфейсу, что и другие
-    // компоненты. Основная цель этого класса - определить интерфейс обёртки для
-    // всех конкретных декораторов. Реализация кода обёртки по умолчанию может
-    // включать в себя  поле для хранения завёрнутого компонента и средства его
-    // инициализации.
     abstract class Decorator : Component
     {
         protected Component _component;
         public Decorator(Component component)
         {
-            this._component = component;
+            _component = component;
         }
         public void SetComponent(Component component)
         {
-            this._component = component;
+            _component = component;
         }
-        // Декоратор делегирует всю работу обёрнутому компоненту.
         public override string Operation()
         {
-            if (this._component != null)
-            {
-                return this._component.Operation();
-            }
-            else
-            {
-                return string.Empty;
-            }
+            if (_component != null) return _component.Operation();
+            else return string.Empty;
         }
     }
-    // Конкретные Декораторы вызывают обёрнутый объект и изменяют его результат
-    // некоторым образом.
     class ConcreteDecoratorA : Decorator
     {
-        public ConcreteDecoratorA(Component comp) : base(comp)
-        {
-        }
-        // Декораторы могут вызывать родительскую реализацию операции, вместо
-        // того, чтобы вызвать обёрнутый объект напрямую. Такой подход упрощает
-        // расширение классов декораторов.
+        public ConcreteDecoratorA(Component comp) : base(comp) { }
         public override string Operation()
         {
             return $"ConcreteDecoratorA({base.Operation()})";
         }
     }
-    // Декораторы могут выполнять своё поведение до или после вызова обёрнутого
-    // объекта.
     class ConcreteDecoratorB : Decorator
     {
         public ConcreteDecoratorB(Component comp) : base(comp)
@@ -103,6 +83,7 @@ namespace DesignPatterns_Decorator
             client.ClientCode(decorator2);
             client.ClientCode(decorator3);
             client.ClientCode(decorator4);
+            ReadKey();
         }
     }
 }
